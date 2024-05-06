d01d98e949c6c516a408c78f584069fdbaa3df4a
ProoRajat <rajatbharat67@gmail.com>
2024-05-07 00:06:25 +0530

    Update Even_Fibonacci_Numbers.py

--- Even_Fibonacci_Numbers.py
+++ Even_Fibonacci_Numbers.py
@@ -1,3 +1,8 @@
+''' Known Problems to fix
+- number should be an integer and gerater and equal to 2
+- output when number is 2
+'''
+
 def Fibonacci_Series(num):
     series = [1, 2]
     while series[-1] < num:
@@ -6,8 +11,16 @@ def Fibonacci_Series(num):
         del series[-1]
     return series
 
-def sum_of_even_numbers():
+def sum_of_even_numbers(num):
+    series = Fibonacci_Series(num)
+    sum = 0
+    for i in series:
+        if i%2 == 0:
+            sum += i
+    return sum
+
 
 number = int(input("Last term should not exceed value of:- "))
 
-print(Fibonacci_Series(number))
+print("Fibonacci Series is : ",Fibonacci_Series(number))
+print("Sum of even numbers in the series : ", sum_of_even_numbers(number))
